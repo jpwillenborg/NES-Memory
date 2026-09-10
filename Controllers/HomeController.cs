0a3bc3ee528f0208053f448cd046e6c865f3e9fb
JP Willenborg <jpwillenborg@users.noreply.github.com>
2026-09-10 01:01:47 -0500

    ad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,12 @@ namespace NES_Box_Art.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            // This automatically redirects traffic from "/" to "/Grid"
+            return RedirectToAction("Index", "Grid"); 
+        }
+
         private readonly IConfiguration _config;
 
         public HomeController(IConfiguration config)
